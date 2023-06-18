e175c255bb1dabd9d196e774e52f3f49ae499432
karthik <karthikprabhu23223@gmail.com>
2023-06-18 18:07:16 +0530

    completed favorites

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+// ignore: unnecessary_import
 import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
